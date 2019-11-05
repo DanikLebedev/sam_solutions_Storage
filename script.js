581657982a7153d2ba1552b156bd4d6d73f68e10
DanikLebedev <danik_lebedev1999@mail.ru>
2019-11-05 16:45:16 +0300

    Done

--- script.js
+++ script.js
@@ -1,7 +1,31 @@
 const colorChangeBtn = document.getElementById('change-color');
-const title = document.querySelector('h1');
+// const redBtn = document.getElementById('change-red');
+const body = document.querySelector('body');
+
+
+// redBtn.addEventListener('click', () => {
+//     localStorage.setItem('font-color', 'red');
+//     location.reload()
+// })
 
 colorChangeBtn.addEventListener('click', () => {
-    localStorage.setItem('color','green');
-    title.style.backgroundColor = `${localStorage.color}`;
+    localStorage.setItem('color', 'green');
+    location.reload()
 });
+
+const fontColor = localStorage.getItem('font-color');
+body.style.color = fontColor;
+
+const color = localStorage.getItem('color');
+body.style.backgroundColor = color;
+
+// if (!window.localStorage.getItem('color')) {
+//     location.reload()
+// }
+//
+// if (!window.localStorage.getItem('font-color')) {
+//     location.reload()
+// }
+
+
+
